a61d6b8bbb01d1d5c4b0dbbfe1db5f76ca61bb63
Jakub Pavlík <severus@post.cz>
2018-11-05 20:02:23 +0100

    rewrite tests to use MockMvc

--- src/test/java/calendar/DayEndpointTest.java
+++ src/test/java/calendar/DayEndpointTest.java
@@ -4,41 +4,44 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class DayEndpointTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     @Test
     public void shouldReturnExpectedDayData() throws Exception {
-        assertThat(this.restTemplate.getForObject(this.endpointPath(),
-                String.class)).isEqualTo("{\"date\":\"2018-10-18\"}");
+        this.mockMvc.perform(get(this.endpointPath())).andExpect(status().isOk())
+                .andExpect(content().string("{\"date\":\"2018-10-18\"}"));
     }
 
     @Test
     public void shouldReturnExpectedDayData2() throws Exception {
-        assertThat(this.restTemplate.getForObject(this.endpointPath(2000, 7, 5),
-                String.class)).isEqualTo("{\"date\":\"2000-07-05\"}");
+        this.mockMvc.perform(get(this.endpointPath(2000, 7, 5))).andExpect(status().isOk())
+                .andExpect(content().string("{\"date\":\"2000-07-05\"}"));
     }
 
     @Test
     public void monthOverflow() throws Exception {
         // this is not behaviour we love, but Java behaves this way and there's no important requirement to change this
-        assertThat(this.restTemplate.getForObject(this.endpointPath(2000, 7, 33),
-                String.class)).isEqualTo("{\"date\":\"2000-08-02\"}");
+        this.mockMvc.perform(get(this.endpointPath(2000, 7, 33))).andExpect(status().isOk())
+                .andExpect(content().string("{\"date\":\"2000-08-02\"}"));
     }
 
     protected String endpointPath() {
@@ -46,7 +49,7 @@ public class DayEndpointTest {
     }
 
     protected String endpointPath(int year, int month, int day) {
-        return "http://localhost:" + port + "/api/v1/calendars/general-en/" +
+        return "/api/v1/calendars/general-en/" +
                 String.format("%d/%d/%d", year, month, day);
     }
 }
